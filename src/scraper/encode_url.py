0805f94aa5cf8e6ec767987f1f02d57649ca3578
tenera <suitboy23@gmail.com>
2025-12-07 17:05:19 +0300

    ruff

--- src/scraper/encode_url.py
+++ src/scraper/encode_url.py
@@ -1,36 +1,39 @@
-CS2_MARKET_ITEM_URL_NAMES : dict [str, str] = {
-  ' ': '%20',
-  '%': '%25',
-  '&': '%26',
-  '+': '%2B',
-  '?': '%3F',
-  '#': '%23',
-  '=': '%3D',
-  '/': '%2F',
-  '\\': '%5C',
-  '"': '%22',
-  "'": '%27',
-  '<': '%3C',
-  '>': '%3E',
-  '|': '%7C',  #VERY IMPORTANT for CS2
-  ':': '%3A',
-  ';': '%3B',
-  '@': '%40',
-  '(': '%28',  # For wear grades
-  ')': '%29',  # For wear grades
-  '[': '%5B',
-  ']': '%5D',
-  '{': '%7B',
-  '}': '%7D',
-  '`': '%60',
-  '^': '%5E',
-  '~': '%7E',
-  "★" : "%E2%98%85"
+CS2_MARKET_ITEM_URL_NAMES: dict[str, str] = {
+    " ": "%20",
+    "%": "%25",
+    "&": "%26",
+    "+": "%2B",
+    "?": "%3F",
+    "#": "%23",
+    "=": "%3D",
+    "/": "%2F",
+    "\\": "%5C",
+    '"': "%22",
+    "'": "%27",
+    "<": "%3C",
+    ">": "%3E",
+    "|": "%7C",  # VERY IMPORTANT for CS2
+    ":": "%3A",
+    ";": "%3B",
+    "@": "%40",
+    "(": "%28",  # For wear grades
+    ")": "%29",  # For wear grades
+    "[": "%5B",
+    "]": "%5D",
+    "{": "%7B",
+    "}": "%7D",
+    "`": "%60",
+    "^": "%5E",
+    "~": "%7E",
+    "★": "%E2%98%85",
 }
+
 
 async def encode_cs2_item_name(item_name: str) -> str:
     encoded_name = ""
     for letter in item_name:
-        encoded_name += CS2_MARKET_ITEM_URL_NAMES.get(letter, letter) # default for get fallback if no letter in dict
-    
+        encoded_name += CS2_MARKET_ITEM_URL_NAMES.get(
+            letter, letter
+        )  # default for get fallback if no letter in dict
+
     return encoded_name
--- src/tests/test_encoding.py
+++ src/tests/test_encoding.py
@@ -3,12 +3,25 @@ from src.scraper.encode_url import encode_cs2_item_name
 import asyncio
 import pytest
 
+
 @pytest.mark.asyncio
 async def test_encoding():
-    assert await encode_cs2_item_name("AK-47 | Redline (Field-Tested)") == "AK-47%20%7C%20Redline%20%28Field-Tested%29"
-    assert await encode_cs2_item_name("AWP | Dragon Lore (Factory New)") == "AWP%20%7C%20Dragon%20Lore%20%28Factory%20New%29"
-    assert await encode_cs2_item_name("★ Karambit | Doppler (Factory New)") == "%E2%98%85%20Karambit%20%7C%20Doppler%20%28Factory%20New%29"
-    assert await encode_cs2_item_name("Dreams & Nightmares Case") == "Dreams%20%26%20Nightmares%20Case"
+    assert (
+        await encode_cs2_item_name("AK-47 | Redline (Field-Tested)")
+        == "AK-47%20%7C%20Redline%20%28Field-Tested%29"
+    )
+    assert (
+        await encode_cs2_item_name("AWP | Dragon Lore (Factory New)")
+        == "AWP%20%7C%20Dragon%20Lore%20%28Factory%20New%29"
+    )
+    assert (
+        await encode_cs2_item_name("★ Karambit | Doppler (Factory New)")
+        == "%E2%98%85%20Karambit%20%7C%20Doppler%20%28Factory%20New%29"
+    )
+    assert (
+        await encode_cs2_item_name("Dreams & Nightmares Case")
+        == "Dreams%20%26%20Nightmares%20Case"
+    )
 
 
 @pytest.mark.asyncio
