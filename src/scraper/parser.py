f2aa2819298a34cb3b3ab6bb2129974227f3f8ec
danielloda89 <43823047+danielloda89@users.noreply.github.com>
2025-12-09 20:26:29 +0000

    added calculations module

--- src/scraper/parser.py
+++ src/scraper/parser.py
@@ -1,18 +1,42 @@
-import aiohttp
-
+from calculations import calculate_median_price
 from encode_url import encode_cs2_item_name
 import pytest
 import asyncio
 import json
+import aiohttp
 
 async def scrap(target_item_name: str):
-    line = f"https://steamcommunity.com/market/search/render?&norender=1&start=0&count=99&query={encode_cs2_item_name(target_item_name)}"   
-    line2 = f"https://steamcommunity.com/market/listings/730/{encode_cs2_item_name("M249 | System Lock (Factory New)")}/render?currency=3&start=0"
+    #line = f"https://steamcommunity.com/market/search/render?&norender=1&start=0&count=99&query={encode_cs2_item_name(target_item_name)}"   
+    line2 = f"https://steamcommunity.com/market/listings/730/{encode_cs2_item_name(target_item_name)}/render?currency=1&start=0"
+    #line3 = f"https://steamcommunity.com/market/priceoverview/?country=RU&currency=3&appid=578080&market_hash_name={encode_cs2_item_name(target_item_name)}"
     async with aiohttp.ClientSession() as session:
-        async with session.get(line) as response:
-            
+        async with session.get(line2) as response:
             data = await response.json()
-            #print(data["results"][0]["sell_price"])
-            print(data)
+            return data
+              
 
-asyncio.run(scrap("Dreams & Nightmares Case"))
+def get_item_data(cs_item):
+    price_list = []
+
+    for item in cs_item.get("listinginfo"):    
+        if cs_item["listinginfo"][item]["currencyid"] != 2003:
+            price_list.append(cs_item["listinginfo"][item]["converted_price"]/100)
+        else:
+            price_list.append(cs_item["listinginfo"][item]["price"]/100)
+            
+    return price_list
+
+
+
+
+async def check():
+    listing = ["Prisma Case"]
+    x = await scrap("Prisma Case")
+    #print(x)
+    prices = get_item_data(x)
+    print(prices)
+    calculated = calculate_median_price(prices)
+    print(calculated)
+   
+
+asyncio.run(check())
